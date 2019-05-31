ed64d7c005d5e0b69c903788c24753b62a76e05d
primeBias <david.jhoo@gmail.com>
2019-05-31 09:27:43 -0400

    Refactored sci_to_d.cpp

--- chapter07/overflow/sci_to_d.cpp
+++ chapter07/overflow/sci_to_d.cpp
@@ -21,25 +21,11 @@ void putback_str(string s)
 		cin.putback(s[i]);
 }
 
-string sci_to_d(string s)
+// Prepocess the significand by removing the decimal and negative signs if any.
+// Returns flags for if the significand was negative or had no integer value.
+vector<bool> remove_dec_significand(string& significand)
 {
-	putback_str(s);
-
-	string significand;
-	char ch;
-
-	significand = build_int();
-	cout << "significand= " << significand << "\tsignificand.size()= " << significand.size() << endl;
-	cin.get(ch);
-	if (ch != 'e') throw runtime_error("sci_to_d(): Expected 'e' for exponential.");
-	int exponential;
-	cin >> exponential;
-	if (!cin) throw runtime_error("sci_to_d(): Invalid input for variable `exponential`.");
-	if (exponential == 0) throw runtime_error("sci_to_d(): Illegal: Exponential value is in (-1,1) range.");
-	cin.ignore();			// this line is necessary to stop invalid input on next loop.
-	cout << "starting exponential= " << exponential << endl;
-
-	int decimal_index = 0;
+	vector<bool> ret;
 	bool neg = false;
 	bool no_int = false;
 	if (significand[0] == '-')
@@ -49,12 +35,19 @@ string sci_to_d(string s)
 	}
 	if (significand[0] == '.') 
 	{
-		decimal_index = -1;
 		no_int = true;
 		significand.erase(0,1);
 	}
 	if (significand[1] == '.') significand.erase(1,1);
-	cout << "significand with no decimal= " << significand << endl;
+
+	ret.push_back(neg);
+	ret.push_back(no_int);
+
+	return ret;
+}
+
+void sci_to_d_exp_grtr_zero(int& decimal_index, int& exponential, string& significand)
+{
 	while (exponential > 0)
 	{
 		if (decimal_index == significand.size()-1 && exponential > 0)		
@@ -74,8 +67,10 @@ string sci_to_d(string s)
 			--exponential;
 		}
 	}
+}
 
-	++exponential; 		// removing decimal is equivalent to applying factor of 10 in below case.
+void sci_to_d_exp_less_zero(int& decimal_index, int& exponential, string& significand)
+{
 	while (exponential < 0)
 	{
 		significand = '0' + significand;
@@ -83,7 +78,44 @@ string sci_to_d(string s)
 	}
 	
 	if (exponential == 0) significand = '.' + significand;
+}
 
+void get_significand_exponential(string& significand, int& exponential)
+{
+	char ch;
+	significand = build_int();
+	cout << "significand= " << significand << "\tsignificand.size()= " << significand.size() << endl;
+	cin.get(ch);
+	if (ch != 'e') throw runtime_error("sci_to_d(): Expected 'e' for exponential.");
+	cin >> exponential;
+	if (!cin) throw runtime_error("sci_to_d(): Invalid input for variable `exponential`.");
+	if (exponential == 0) throw runtime_error("sci_to_d(): Illegal: Exponential value is in (-1,1) range.");
+	cin.ignore();			// this line is necessary to stop invalid input on next loop.
+}
+
+string sci_to_d(string s)
+{
+	putback_str(s);
+
+	string significand;
+	int exponential;
+	get_significand_exponential(significand, exponential);
+
+	cout << "starting exponential= " << exponential << endl;
+
+	int decimal_index = 0;
+	
+	vector<bool> preprocessed_significand = remove_dec_significand(significand); 
+	
+	bool neg = preprocessed_significand[0];
+	bool no_int = preprocessed_significand[1];
+	if (no_int) decimal_index--;		// necessary adjustment for case of no_int		
+	cout << "significand with no decimal= " << significand << endl;
+
+	sci_to_d_exp_grtr_zero(decimal_index, exponential, significand);
+	++exponential; 		// removing decimal is equivalent to applying factor of 10 in below case.
+
+	sci_to_d_exp_less_zero(decimal_index, exponential, significand);
 	string ret = significand;
 	if (neg) ret = '-' + ret;
 
